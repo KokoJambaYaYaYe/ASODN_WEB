615ca8fbaa2594e54d62b2c73cee0c3437a3cf71
Koko Jamba <xxxkokojambayayayexxx@gmail.com>
2026-08-01 19:19:36 +0300

    sf

--- AuthSystem.API/Controller/AuthLogoutController.cs
+++ AuthSystem.API/Controller/AuthLogoutController.cs
@@ -23,8 +23,8 @@ public class AuthLogoutController : ControllerBase
         _authorizationManager = authorizationManager;
     }
 
-    [HttpGet("connect/logout")]
-    [HttpPost("connect/logout")]
+    [HttpGet("~/connect/logout")]
+    [HttpPost("~/connect/logout")]
     public async Task<IActionResult> Logout([FromQuery] string returnUrl = "/")
     {
         // ЯВНО просим ASP.NET Core прочитать нашу сессионную куку
