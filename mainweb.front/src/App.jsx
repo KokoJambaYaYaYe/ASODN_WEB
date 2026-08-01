111ada14abf1612dd0140592b43a814072e0cf78
Koko Jamba <xxxkokojambayayayexxx@gmail.com>
2026-08-01 12:23:58 +0300

    sf

--- mainweb.front/src/App.jsx
+++ mainweb.front/src/App.jsx
@@ -47,7 +47,7 @@ function App() {
 
             const baseAuthApiUrl = import.meta.env.VITE_AUTH_API_URL;
 
-            fetch(`${baseAuthApiUrl}/profile/info`, {
+            fetch(`${baseAuthApiUrl}authsystem_api/profile/info`, {
                     method: 'GET',
                     credentials: 'include' // КРИТИЧНО для передачи сессионной куки
                 })
